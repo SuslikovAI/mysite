02e5ba0a8887eb0a90bbd3d38e3830a59725b75c
SuslikovAI <57615377+SuslikovAI@users.noreply.github.com>
2019-12-16 11:02:19 +0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<style>
+#body
+{
+	background-size: 100%;
+background-image:url(main.jpg);
+}
+</style>
+	<title>main</title>
+	<meta charset="utf-8">
+</head>
+<body id="body">
+	<p>If you want to login with a different name go <a href="logout.php">Exit</a></p>
+	<p>BAN</p>
+	<form action="ban.php" method="post">
+	<p>Name</p>
+	<input type="text" name="login" placeholder="Name"><br>
+		<input type="submit" value="add"><br></form>
+
+	</body>
+</html>
