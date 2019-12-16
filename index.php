6e679816d5379e818352ae544ecb92835a44d6e7
SuslikovAI <57615377+SuslikovAI@users.noreply.github.com>
2019-12-16 11:05:57 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gateway</title>
+
+	<meta charset="utf-8">
+	<style>
+	#body{
+    background-image: url(image.jpg);
+    background-size: 100%;
+	}
+	#comment{
+    color: grey;
+  	padding: 30px;
+ 	text-align: center;
+ 	font-size: 35px;
+    width: 95%;
+	}
+  #column1 {
+  float: left;
+  width: 48%;
+}
+  #column2 {
+
+  width: 47%;
+  float: right;
+  }
+  .row: {
+  width:95%;
+}
+</style>
+</head>
+<body id="body">
+<div id="comment">
+Этот сайт для сохранения и передачи такого вида устного народного творчества, как Анекдоты.
+На этом сайте вы можете увидеть лучшие произведения, набравшие наибольший рейтинг.
+Сюда могут зайти простые пользователи чтобы посмотреть и найти что-то, что придется по-вкусу. 
+Или же может зайти администратор...
+<div class="row">
+<div id="column1">Если пользователь не зарегистрирован, он заходит в страницу для <a href="registration.html">регистрации</a> и регистрируется.
+</div>
+<div id="column2">Если пользователь уже зарегистрован он может <a href="login.html">залогинится</a> и посмотреть выборку Анекдотов.<br></div>
+</div></div>
+</body>
+</html>
